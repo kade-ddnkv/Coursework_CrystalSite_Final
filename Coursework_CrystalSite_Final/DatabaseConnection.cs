7fcf561b4a07215ff5b5b5d6fcbc97990d86a75d
Кирилл Дудников <kddudnikov@edu.hse.ru>
2022-05-13 00:33:29 +0300

    Публикация в Azure.

--- Coursework_CrystalSite_Final/DatabaseConnection.cs
+++ Coursework_CrystalSite_Final/DatabaseConnection.cs
@@ -6,8 +6,13 @@
     public class DatabaseConnection
     {
         /// <summary>
-        /// Строка для подключения к базе данных. Например, через Dapper.
+        /// Строка для подключения к локальной базе данных. Например, через Dapper.
         /// </summary>
-        public const string ConnectionString = "Server=localhost; Database=Crystal; Integrated Security=True; TrustServerCertificate=True;";
+        public const string ConnectionStringObsolete = "Server=localhost; Database=Crystal; Integrated Security=True; TrustServerCertificate=True;";
+
+        /// <summary>
+        /// Строка подключения к базе данных в Azure.
+        /// </summary>
+        public const string ConnectionString = "Server=tcp:crystal.database.windows.net,1433;Initial Catalog=Crystal;Persist Security Info=False;User ID=rootcrystal;Password=73u$7$BmC7VW;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 }
