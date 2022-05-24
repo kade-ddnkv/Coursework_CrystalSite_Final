282a583237da9a453d0ca7576c1f2e72aca1c62c
kade-ddnkv <88973774+kade-ddnkv@users.noreply.github.com>
2022-05-24 13:58:10 +0300

    Скрыта строка подключения к БД.

--- Coursework_CrystalSite_Final/DatabaseConnection.cs
+++ Coursework_CrystalSite_Final/DatabaseConnection.cs
@@ -13,6 +13,6 @@
         /// <summary>
         /// Строка подключения к базе данных в Azure.
         /// </summary>
-        public const string ConnectionString = "Server=tcp:crystal.database.windows.net,1433;Initial Catalog=Crystal;Persist Security Info=False;User ID=rootcrystal;Password=73u$7$BmC7VW;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        public const string ConnectionString = "[ДАННЫЕ УДАЛЕНЫ]";
     }
 }
